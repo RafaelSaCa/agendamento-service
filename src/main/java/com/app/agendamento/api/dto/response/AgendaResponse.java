587044e2915006722bdd29e5d6223c2b96c5f473
Rafael Saca <raffaellsaca@gmail.com>
2024-06-10 21:52:23 -0300

    feat: Response de agenda

---
+++ src/main/java/com/app/agendamento/api/dto/response/AgendaResponse.java
@@ -0,0 +1,20 @@
+package com.app.agendamento.api.dto.response;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgendaResponse {
+
+    private Long id;
+    private String descricao;
+    private LocalDateTime horario;
+    private PacienteResponse paciente;
+}
