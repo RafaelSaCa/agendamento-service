a2d34c751acacb671cb8b7ef40373b1469e3b2aa
Rafael Saca <raffaellsaca@gmail.com>
2024-07-25 23:43:19 -0300

    feat: Service Procedimento

---
+++ src/main/java/com/app/agendamento/domain/service/ProcedimentoService.java
@@ -0,0 +1,44 @@
+package com.app.agendamento.domain.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.app.agendamento.domain.entity.Procedimento;
+import com.app.agendamento.domain.repository.ProcedimentoRepository;
+import com.app.agendamento.exception.NegocioException;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ProcedimentoService {
+
+    private final ProcedimentoRepository repository;
+
+    public Procedimento salvar (Procedimento procedimento){
+        return repository.save(procedimento);
+    }
+
+    public List<Procedimento> listar (){
+        return repository.findAll();
+    }
+
+    public Optional<Procedimento> buscarPorId(Long id){
+        return repository.findById(id);
+    }
+
+    public Procedimento alterar (Long id, Procedimento procedimento){
+        Optional<Procedimento> procedimentoExistente = this.buscarPorId(id);
+
+        if ( procedimentoExistente.isEmpty()){
+            throw new NegocioException(id);
+        }
+
+        procedimento.setId(id);
+        return salvar(procedimento);
+    }
+}
