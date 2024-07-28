44076108cefc3c268e81043470598f16a1ba545c
Rafael Saca <raffaellsaca@gmail.com>
2024-07-28 20:57:07 -0300

    feat: Mapper Procedimento

---
+++ src/main/java/com/app/agendamento/api/dto/mapper/ProcedimentoMapper.java
@@ -0,0 +1,35 @@
+package com.app.agendamento.api.dto.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.app.agendamento.api.dto.request.ProcedimentoRequest;
+import com.app.agendamento.api.dto.response.ProcedimentoResponse;
+import com.app.agendamento.domain.entity.Procedimento;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProcedimentoMapper {
+
+    private final ModelMapper mapper;
+
+    public Procedimento toProcedimento (ProcedimentoRequest procedimentoRequest){
+        return mapper.map(procedimentoRequest, Procedimento.class);
+    }
+
+    public ProcedimentoResponse toProcedimentoResponse (Procedimento procedimento){
+        return mapper.map(procedimento, ProcedimentoResponse.class);
+    }
+
+    public List<ProcedimentoResponse> toPacienteResponseList (List<Procedimento> procedimentos){
+        return procedimentos.stream()
+                .map(this::toProcedimentoResponse)
+                .collect(Collectors.toList());
+    }
+
+}
