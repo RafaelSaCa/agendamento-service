587164fd69df2cf3c4f56884eeddd25ab277eba2
Rafael Saca <raffaellsaca@gmail.com>
2024-07-30 23:46:08 -0300

    fix: AgendaMapper metodo para converter a entidade em dto

--- src/main/java/com/app/agendamento/api/dto/mapper/AgendaMapper.java
+++ src/main/java/com/app/agendamento/api/dto/mapper/AgendaMapper.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Component;
 import com.app.agendamento.api.dto.request.AgendaRequest;
 import com.app.agendamento.api.dto.response.AgendaResponse;
 import com.app.agendamento.domain.entity.Agenda;
+import com.app.agendamento.domain.repository.MedicoRepository;
+import com.app.agendamento.domain.repository.PacienteRepository;
+import com.app.agendamento.domain.repository.ProcedimentoRepository;
+import com.app.agendamento.exception.NegocioException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +21,9 @@ import lombok.RequiredArgsConstructor;
 public class AgendaMapper {
 
     private final ModelMapper mapper;
+    private final MedicoRepository medicoRepository;
+    private final PacienteRepository pacienteRepository;
+    private final ProcedimentoRepository procedimentoRepository;
 
     public Agenda toAgenda(AgendaRequest agendaRequest) {
         return mapper.map(agendaRequest, Agenda.class);
@@ -32,4 +39,15 @@ public class AgendaMapper {
                 .collect(Collectors.toList());
     }
 
+    public Agenda convertToAgendaResponse(AgendaRequest agendaRequest){
+        Agenda agenda = mapper.map(agendaRequest, Agenda.class);
+        agenda.setHorario(agendaRequest.getHorario());
+        agenda.setProcedimento(procedimentoRepository.findById(agendaRequest.getProcedimentoId()).orElseThrow(() -> new NegocioException("Procedimento não encontrado!")));
+        agenda.setMedico(medicoRepository.findById(agendaRequest.getMedicoId()).orElseThrow(() -> new NegocioException("Médico não encontrado!")));
+        agenda.setPaciente(pacienteRepository.findById(agendaRequest.getPacienteId()).orElseThrow(() -> new NegocioException("Paciente não encontrado!")));
+
+        return agenda;
+    }
+
+   
 }
