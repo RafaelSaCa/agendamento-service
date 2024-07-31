6217a7b5c45bc609e5d0be990a744d9ba1799487
Rafael Saca <raffaellsaca@gmail.com>
2024-07-30 23:44:00 -0300

    feat: Entidade Agenda - chaves estrangeiras de procedimento, paciente e medico

--- src/main/java/com/app/agendamento/domain/entity/Agenda.java
+++ src/main/java/com/app/agendamento/domain/entity/Agenda.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -21,12 +22,20 @@ public class Agenda {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String descricao;
 
     @Column(name = "data_hora")
     private LocalDateTime horario;
 
     @ManyToOne
+    @JoinColumn(name = "procedimento_id")
+    private Procedimento procedimento;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+    
+    @ManyToOne
+    @JoinColumn(name = "paciente_id")
     private Paciente paciente;
 
 }
